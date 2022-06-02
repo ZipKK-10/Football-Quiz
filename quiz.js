0bd7bc8d60fd4fee24726b9c9a856108b3ed9839
Krzychu <krzysztof.kerlin@ptgit.home.pl>
2022-06-02 21:52:24 +0200

    Added questions and answers

--- quiz.js
+++ quiz.js
@@ -1,6 +1,12 @@
 const startPage = document.getElementById('start')
 const quizPage = document.getElementById('quiz')
 const startButton = document.getElementById('start-btn')
+const questionTag = document.getElementById('question')
+const answersContainer = document.getElementById('answers')
+const answerATag = document.getElementById('answerA')
+const answerBTag = document.getElementById('answerB')
+const answerCTag = document.getElementById('answerC')
+const answerDTag = document.getElementById('answerD')
 
 //start quiz//
 startButton.addEventListener('click', startQuiz)
@@ -9,3 +15,107 @@ function startQuiz() {
     startPage.style.display = "none"
     quizPage.style.display = "flex"
 }
+
+//questions and answers//
+let questions = [{
+        question: "Kto został królem strzelców na mundialu we Francji w 1998 roku?",
+        answers: ["Davor Šuker", "Dennis Bergkamp", "Ronaldo Nazario", "Thierry Henry"],
+        correct: answerA,
+    },
+    {
+        question: "Kto zagrał w finale mundialu w 2006 roku?",
+        answers: ["Francja-Brazylia", "Włochy-Francja", "Brazylia-Niemcy", "Hiszpania-Holandia"],
+        correct: answerB,
+    },
+    {
+        question: "Kto wygrał ostatni Puchar Zdobywcow Pucharów?",
+        answers: ["Parma", "Olympique Marseille", "Feyenoord", "Lazio"],
+        correct: answerD,
+    },
+    {
+        question: "Który trener zdobył UEFA Ligę Mistrzów i Mistrzostwo Świata?",
+        answers: ["Vicente del Bosque", "Carlo Ancelotti", "Luiz Felipe Scolari", "Didier Deschamps"],
+        correct: answerA,
+    },
+    {
+        question: "W którym klubie nie grał Christian Vieri?",
+        answers: ["Roma", "Milan", "Inter", "Juventus"],
+        correct: answerA,
+    },
+    {
+        question: "Kto jest 2-krotnym Mistrzem Świata?",
+        answers: ["Romario", "Cafu", "Rivaldo", "Roberto Carlos"],
+        correct: answerB,
+    },
+    {
+        question: "Który klub nie posiada honorowej odznaki Ligi Mistrzów?",
+        answers: ["AC Milan", "FC Barcelona", "Manchester United", "Ajax Amsterdam"],
+        correct: answerC,
+    },
+    {
+        question: "Która z drużyn nigdy nie została Mistrzem Niemiec?",
+        answers: ["Werder Brema", "Hertha Berlin", "Bayer 04 Leverkusen", "TSV 1860 Monachium"],
+        correct: answerC,
+    },
+    {
+        question: "Który piłkarz nie grał w MLS?",
+        answers: ["David Beckham", "Lothar Matthäus", "Kaká", "Gabriel Batistuta"],
+        correct: answerD,
+    },
+    {
+        question: "Po kim Ronaldinho przejął numer 10 w PSG?",
+        answers: ["Mikeal Arteta", "Pedro Pauleta", "Nicolas Anelka", "Jay-Jay Okocha"],
+        correct: answerD,
+    },
+    {
+        question: "Która reprezentacja zagrała w meczu finałowym mundialu?",
+        answers: ["Portugalia", "Bułgaria", "Szwecja", "Jugosławia"],
+        correct: answerC,
+    },
+    {
+        question: "W którym roku Holandia zdobyła Mistrzostwo Europy?",
+        answers: ["1992", "1988", "1996", "1980"],
+        correct: answerB,
+    },
+    {
+        question: "Który klub ma najwięcej tytułów Mistrza Brazylii?",
+        answers: ["SE Palmeiras", "Fluminense FC", "SC Corinthians", "Vasco da Gama"],
+        correct: answerA,
+    },
+    {
+        question: "Ile sezonów Sir Alex Ferguson prowadził Manchester United?",
+        answers: ["20", "26", "29", "24"],
+        correct: answerB,
+    },
+    {
+        question: "Który piłkarz nie zdobył Złotej Piłki?",
+        answers: ["Andriy Shevchenko", "Micheal Owen", "Steven Gerrard", "Zinédine Zidane"],
+        correct: answerC,
+    },
+    {
+        question: "Kogo Roman Abramowicz ściągnął do Chelsea najpóźniej?",
+        answers: ["Joe Cole", "Juan Sebastian Veron", "Didier Drogba", "Claude Makalele"],
+        correct: answerC,
+    },
+    {
+        question: "Z którego kraju pochodzi klub Vissel Kobe?",
+        answers: ["Korei Południowej", "Wietnamu", "Chin", "Japonii"],
+        correct: answerD,
+    },
+    {
+        question: "Kto ma najwięcej występów w koszulce Realu Madryt?",
+        answers: ["Raúl", "Fernando Hierro", "Iker Casillas", "Guti"],
+        correct: answerA,
+    },
+    {
+        question: "Kto nie będzie współgospodarzem EURO 2021?",
+        answers: ["Szkocja", "Belgia", "Rosja", "Dania"],
+        correct: answerB,
+    },
+    {
+        question: "Które drużyny 3-krotnie zdobyły Mistrzostwo Europy?",
+        answers: ["Niemcy i Hiszpania", "Niemcy i Włochy", "Francja i Włochy", "Francja i Hiszpania"],
+        correct: answerA,
+    },
+]
+
